db29b6762efff03c29d8c316297e00052ef97fcf
PDochev <plamendochev0@gmail.com>
2025-02-22 16:25:57 +0000

    Add environment variable templates and implement DB_PATH handling

--- base-concert-venue/lib/db/constants.ts
+++ base-concert-venue/lib/db/constants.ts
@@ -1,4 +1,9 @@
 export const venueCapacity = 400;
 
 // this will eventually use environment variables
-export const getDbPath = (): string => "db";
+export const getDbPath = (): string => {
+  if (!process.env.DB_PATH) {
+    throw new Error("Missing DB_PATH environment variable");
+  }
+  return process.env.DB_PATH; // return the DB_PATH value
+};
